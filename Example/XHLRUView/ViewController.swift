27da4d405c0f91c6f85a063cd32fb8de7924ade5
Lxh93 <894365394@qq.com>
2021-03-27 18:40:45 +0800

    Initial commit

---
+++ Example/XHLRUView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  XHLRUView
+//
+//  Created by Lxh93 on 03/27/2021.
+//  Copyright (c) 2021 Lxh93. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
